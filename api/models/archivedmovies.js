4762e6aec2dd9a705673500ee7df3d02a0689568
nicolae.mihalache@titanium-soft.com <nicolae.mihalache@titanium-soft.com>
2019-05-03 15:35:07 +0300

    deleted unessecary stuff

--- api/models/archivedmovies.js
+++ api/models/archivedmovies.js
@@ -6,8 +6,6 @@ const archivedMovies = db.define('archivedMovies', {
   movieId: Sequelize.INTEGER,
   userId: Sequelize.INTEGER
 }, { freezeTableName: true });
-archivedMovies.associate = function (models) {
-  // associations can be defined here
-};
+
 
 module.exports = archivedMovies;
